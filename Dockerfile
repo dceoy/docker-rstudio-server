6b407740b18b721248ace27918c77a6cd3949209
dceoy <d.narsil@gmail.com>
2015-08-27 12:47:59 +0900

    Update config around docker

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,17 @@ FROM ubuntu
 MAINTAINER dceoy
 
 RUN apt-get -y update \
-      && apt-get -y upgrade
-RUN apt-get -y install curl wget r-base gdebi-core \
+      && apt-get -y upgrade \
+      && apt-get -y install curl wget r-base libapparmor1 libcurl4-openssl-dev gdebi-core \
       && apt-cache search r-cran | cut -f 1 -d ' ' | xargs apt-get -y install
-RUN RS_VER=`curl https://s3.amazonaws.com/rstudio-server/current.ver` \
-      && wget https://download2.rstudio.org/rstudio-server-${RS_VER}-amd64.deb \
-      && gdebi rstudio-server-${RS_VER}-amd64.deb \
-      && rm rstudio-server-${RS_VER}-amd64.deb
-RUN apt-get clean
+RUN curl https://s3.amazonaws.com/rstudio-server/current.ver | \
+        xargs -I {} wget http://download2.rstudio.org/rstudio-server-{}-amd64.deb -O rstudio.deb \
+      && gdebi -n rstudio.deb \
+      && rm rstudio.deb \
+      && apt-get clean
+RUN useradd -m -d /home/rstudio rstudio \
+      && echo rstudio:rstudio | chpasswd
+
+EXPOSE 8787
+
+CMD ["/usr/lib/rstudio-server/bin/rserver", "--server-daemonize=0", "--server-app-armor-enabled=0"]
--- fabfile.py
+++ fabfile.py
@@ -6,42 +6,52 @@ from fabric.api import sudo, run, get, task
 
 
 @task
-def test_connect(text=False):
-    if not text:
-        text = 'Succeeded.'
-    run("echo '%s'" % text)
+def new_ssh_user(user, pw, group='sudo'):
+    home = '/home/' + user
+    sudo("useradd -m -d %s %s" % (home, user))
+    sudo("echo '%s:%s' | chpasswd" % (user, pw))
+    sudo("cut -f 1 -d : /etc/group | grep %s || groupadd %s" % (group, group))
+    sudo("usermod -aG %s %s" % (group, user))
+    ssh_keygen(user, home)
+    sudo("chsh %s -s /bin/bash" % user)
+
+
+def ssh_keygen(user, home):
+    sudo("ls %s/.ssh || mkdir %s/.ssh" % (home, home))
+    sudo("ssh-keygen -t rsa -N '' -f %s/.ssh/id_rsa" % home)
+    get(home + '/.ssh/id_rsa', './key/' + user + '_rsa')
+    get(home + '/.ssh/id_rsa.pub', './key/' + user + '_rsa.pub')
+    os.system("chmod 600 ./key/%s" % user + '_rsa')
+    sudo("mv %s/.ssh/id_rsa.pub %s/.ssh/authorized_keys" % (home, home))
+    sudo("chmod 600 %s/.ssh/authorized_keys" % home)
+    sudo("chown -R %s %s/.ssh" % (user, home))
 
 
 @task
-def new_user_rsa(user, group='sudo'):
-    if sudo("id %s" % user, warn_only=True).failed:
-        home = '/home/' + user
-        sudo("useradd -m -g %s -d %s %s" % (group, home, user))
-        sudo("passwd %s" % user)
-        sudo("mkdir %s/.ssh" % home)
-        sudo("ssh-keygen -t rsa -N '' -f %s/.ssh/id_rsa" % home)
-        get(home + '/.ssh/id_rsa', './key/' + user + '_rsa')
-        get(home + '/.ssh/id_rsa.pub', './key/' + user + '_rsa.pub')
-        os.system("chmod 600 ./key/%s" % user + '_rsa')
-        sudo("mv %s/.ssh/id_rsa.pub %s/.ssh/authorized_keys" % (home, home))
-        sudo("chmod 600 %s/.ssh/authorized_keys" % home)
-        sudo("chown -R %s:%s %s/.ssh" % (user, group, home))
+def docker_bash(pid=False):
+    run("docker ps -a")
+    if pid:
+        run("docker exec -it %s bash" % pid)
+    else:
+        run("docker exec -it `docker ps -lq` bash")
 
 
 @task
-def init_with_docker(user, ssh_port=443, excl_port=8787):
+def docker_run_rstudio(port=8787):
+    run("wget https://raw.githubusercontent.com/dceoy/r-server-docker/master/Dockerfile")
+    run("docker build -t dceoy/rstudio .")
+    run("docker run -d -p %d:8787 dceoy/rstudio" % port)
+
+
+@task
+def init_rstudio(user, pw, ssh_port=443, rs_port=8787):
     if user != 'root':
-        new_user_rsa(user)
-        enhance_security(ssh_port, excl_port)
+        new_ssh_user(user, pw)
+        enhance_security(ssh_port, rs_port)
         install_docker(user)
+        docker_run_rstudio(rs_port)
     else:
         print('Set a non-root user.')
-
-
-@task
-def docker_pull_rstudio():
-    run("docker pull ubuntu")
-    run("docker pull dceoy/rstudio")
 
 
 def install_docker(user):
@@ -50,6 +60,8 @@ def install_docker(user):
     sudo("curl -sSL https://get.docker.com/ | sh")
     sudo("usermod -aG docker %s" % user)
     sudo("docker run hello-world")
+    sudo("docker ps -aq | xargs docker rm")
+    sudo("docker rmi hello-world")
 
 
 def enhance_security(ssh_port, excl_port=8787):
@@ -58,10 +70,12 @@ def enhance_security(ssh_port, excl_port=8787):
            's/^#\?\(Port \)22$/\\1' + str(ssh_port) + '/')
     sudo("sed -i -e '%s' -e '%s' -e '%s' /etc/ssh/sshd_config" % rex)
     sudo("service ssh restart")
+    sudo("service ssh status")
     sudo("ufw default deny")
     sudo("ufw allow %s" % ssh_port)
     sudo("ufw allow %s" % excl_port)
     sudo("ufw --force enable")
+    sudo("ufw status")
 
 
 if __name__ == '__main__':
